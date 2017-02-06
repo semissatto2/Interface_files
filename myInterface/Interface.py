d12b9b60a58a0a185b3cacbffe2bac446b9f54e2
Guilherme T. Semissatto <guilherme.semissatto@gae-epics.abtlus.org.br>
2017-02-06 09:09:30 -0200

    Update

--- myInterface/Interface.py
+++ myInterface/Interface.py
@@ -3,10 +3,13 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5 import uic
 import sys
+from epics import caget, PV
 
+# Load UI Files
 Ui_Form_telaInicial, QtBaseClass = uic.loadUiType("telaInicial.ui")
 Ui_Form_epicsInterface, QtBaseClass = uic.loadUiType("epicsInterface.ui")
 
+# Window #1 Class
 class TelaInicial(QMainWindow, Ui_Form_telaInicial):
     def __init__(self, parent=None):
         super(TelaInicial, self).__init__(parent)
@@ -22,7 +25,8 @@ class TelaInicial(QMainWindow, Ui_Form_telaInicial):
     def onClick(self):
         self.epicsInterface = EpicsInterface()
         self.epicsInterface.show()
-
+        
+# Window #2 Class
 class EpicsInterface(QWidget, Ui_Form_epicsInterface):
     def __init__(self, parent=None):
         super(EpicsInterface, self).__init__(parent)
@@ -31,10 +35,23 @@ class EpicsInterface(QWidget, Ui_Form_epicsInterface):
 
         # Bind signal to slot
         self.LinkButtonBack.clicked.connect(self.onClickBack)
-
+        self.pushButton_readPv.clicked.connect(self.readPv)
+        
+    # My slot's
     def onClickBack(self):
         self.close()
-
+        
+    def readPv(self):
+         nome_da_pv = self.lineEdit_readPv.text()
+         if nome_da_pv != '':
+             pv = PV(nome_da_pv)
+             if pv.connected:
+                      pv_value = str(float(pv.value/10))
+                      self.lineEdit_PvValue.setText(pv_value)
+             else:
+                      self.lineEdit_2.PvValue('PV not connected')
+        
+# Init interface
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = TelaInicial()
