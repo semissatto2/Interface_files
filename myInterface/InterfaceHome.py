4cc8c18c50e4188e6d4cdb3a394417e0f2530902
Guilherme Semissatto <semissatto@gmail.com>
2017-02-12 01:01:54 -0200

    Update InterfaceHome.py

--- myInterface/InterfaceHome.py
+++ myInterface/InterfaceHome.py
@@ -181,7 +181,7 @@ class ThreadTempScreen(QtCore.QThread):
         super(ThreadTempScreen, self).__init__(parent)
         
     def run(self):
-        myList = list(range(5))
+        myList = list(range(6))
         while 1:
             A1 = myEpicsHome.getRandom()
             myList[0] = myEpicsHome.getBool()
@@ -191,7 +191,7 @@ class ThreadTempScreen(QtCore.QThread):
             myList[4] = myEpicsHome.getBool()
             myList[4] = myEpicsHome.getBool()
             myList[5] = myEpicsHome.getBool()
-            print (self.myList) # Debugg
+            print (myList) # Debugg
             time.sleep(0.4)
 
 
